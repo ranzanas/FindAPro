2d4cb72df557a5378c0922b27c46e868708c03eb
ranzanas <silwalranzana@gmail.com>
2025-08-14 08:51:38 +0545

    Added validation for login

--- frontend/src/features/auth/login/login.tsx
+++ frontend/src/features/auth/login/login.tsx
@@ -1,32 +1,35 @@
-import { useState, type FormEvent, type ChangeEvent } from "react";
+//import { useState, type FormEvent, type ChangeEvent } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import "./login.css";
 import type { AxiosResponse } from "axios";
 import { loginApi } from "../../../shared/config/api";
+//import Register from "../register/register";
+
+interface ILoginForm {
+  username: string;
+  password: string;
+}
 
 function Login() {
-  const [formData, setFormData] = useState({ username: "", password: "" });
-  const [loading, setLoading] = useState(false);
+  //destructuring
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+    reset,
+  } = useForm<ILoginForm>();
   const navigate = useNavigate();
 
-  function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  }
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    if (loading) {
-      return;
-    }
-    setLoading(true);
-    loginApi(formData)
+  const onSubmit = async (data: ILoginForm) => {
+    await loginApi(data)
       .then((res: AxiosResponse) => {
         localStorage.setItem("token", res.data.token);
         localStorage.setItem("currentUser", JSON.stringify(res.data.user));
         navigate("/home");
       })
       .finally(() => {
-        setLoading(false);
+        reset();
       });
   };
 
@@ -35,30 +38,50 @@ function Login() {
       <div className="header">FindAPro</div>
       <div className="loginBox">
         <h1>Login</h1>
-        <form action="" className="loginForm" onSubmit={handleSubmit}>
+        <form action="" className="loginForm" onSubmit={handleSubmit(onSubmit)}>
           <div className="inputPart">
             <div className="detailSection">
               <span>Username:</span>
               <input
                 type="text"
-                name="username"
-                value={formData.username}
-                onChange={handleChange}
+                {...register("username", {
+                  required: "Username is required",
+                  maxLength: {
+                    value: 10,
+                    message: "Username should be less than 10",
+                  },
+                  minLength: {
+                    value: 3,
+                    message: "Username should be more than 3 characters",
+                  },
+                })}
               />
+
+              {errors.username && (
+                <div className="text-red">
+                  {errors.username.message?.toString()}
+                </div>
+              )}
             </div>
             <div className="detailSection">
               <span>Password:</span>
               <input
                 type="password"
-                name="password"
-                value={formData.password}
-                onChange={handleChange}
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be atleast 6 characters long",
+                  },
+                })}
               />
             </div>
           </div>
 
           <div className="buttonPart">
-            <button type="submit">Log in</button>
+            <button type="submit" disabled={isSubmitting}>
+              Log in
+            </button>
           </div>
           <br />
           <p>
