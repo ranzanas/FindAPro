2a9bc930242cf37cb065642dbe88487a3111b542
ranzanas <silwalranzana@gmail.com>
2025-08-19 05:23:09 +0545

    Added routes

--- Backend/routes/profile.route.js
+++ Backend/routes/profile.route.js
@@ -3,9 +3,10 @@ import {Router } from "express";
 
 const router = Router();
 
-import addEducation, { addExperience } from "../controller/profile.controller.js";
+import addEducation, { addExperience, getEducations, getExperiences } from "../controller/profile.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
 router.post('/addExperience', authMiddleware,  addExperience)
 router.post('/addEducation', authMiddleware, addEducation);
-
+router.get('/listExperience', authMiddleware, getExperiences);
+router.get('/listEducation',authMiddleware, getEducations)
 export default router;
